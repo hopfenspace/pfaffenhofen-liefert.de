df63a4f5449560802b96719311a727dc4f972649
Dennis Denk <dennis@denk.jetzt>
2020-04-06 23:01:58 +0200

    Tabelle hinzugefügt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,81 @@
-import React from 'react'
+import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
-import { Link } from 'gatsby'
-import { MapViewComponent } from '../components/mapview.main'
+import { Link } from 'gatsby';
+import { MapViewComponent } from '../components/mapview.main';
 /* SEO Component with React Helmet */
-import Head from '../components/head'
-import logo from '../../static/teaser.jpg'
+import Head from '../components/head';
+import logo from '../../static/teaser.jpg';
+
+import './tablestyle.scss';
 
 const Index = () => {
-
   const data = useStaticQuery(graphql`
     query {
+      allMapPoints(filter: { approved: { eq: true } }) {
+        nodes {
+          approved
+          address
+          category
+          contact
+          description
+          email
+          id
+          name
+          phone
+          position
+          title
+        }
+      }
       site {
         siteMetadata {
-          title,
-          email,
+          title
+          email
           teaserImage
         }
       }
     }
   `);
 
+  console.log(data);
+
+  /* const entries = useStaticQuery(graphql`
+    query {
+      allMapPoints(filter: { approved: { eq: true } }) {
+        nodes {
+          approved
+          address
+          category
+          contact
+          description
+          email
+          id
+          name
+          phone
+          position
+          title
+        }
+      }
+    }
+  `); */
+
   return (
     <Layout>
       <Head title={data.site.siteMetadata.title} />
-      <div className='index'>
-        <section className='ui vertical very fitted segment' style={{marginTop: '1rem'}}>
-          <div className='ui container'>
-            <h1 className='ui header'>
-              <div className='content'>
+      <div className="index">
+        <section
+          className="ui vertical very fitted segment"
+          style={{ marginTop: '1rem' }}
+        >
+          <div className="ui container">
+            <h1 className="ui header">
+              <div className="content">
                 <img src={logo} className="logo-header" alt="Logo" />
-                <div className='sub header'>
-                  Viele kleine Läden und Dienstleister verwandeln sich in der Corona-Krise in Heimlieferdienste oder bieten To-Go an. Wir zeigen Ihnen, welche. Ist Ihr Geschäft nicht drauf?.{' '}
-                  <Link to={'/add'}>
-                    Helfen sie mit!
-                  </Link>
+                <div className="sub header">
+                  Viele kleine Läden und Dienstleister verwandeln sich in der
+                  Corona-Krise in Heimlieferdienste oder bieten To-Go an. Wir
+                  zeigen Ihnen, welche. Ist Ihr Geschäft nicht drauf?.{' '}
+                  <Link to={'/add'}>Helfen sie mit!</Link>
                 </div>
               </div>
             </h1>
@@ -43,26 +84,51 @@ const Index = () => {
         <section>
           <MapViewComponent />
         </section>
-        <section className='ui vertical segment intro'>
-          <div className='ui text container formcontainer'>
+        <section className="ui vertical segment intro">
+          <div
+            className="ui text container formcontainer"
+            style={{ 'max-width': '1000px' }}
+          >
+            <h2>Alle Teilnehmenden Geschäfte als Liste</h2>
+            <p></p>
+
+            <table>
+              <tr>
+                <th>Name</th>
+                <th>Kategorie</th>
+                <th>Beschreibung</th>
+                <th>Info</th>
+              </tr>
+              {data.allMapPoints.nodes.map((item, i) => (
+                <tr key={item.timestamp}>
+                  <td>{item.title}</td>
+                  <td>{item.category}</td>
+                  <td>{item.description}</td>
+                  <td>{item.contact}</td>
+                </tr>
+              ))}
+            </table>
+
             <h2>Wie lege ich einen Eintrag an?</h2>
             <p>
-              Sie können weitere Einträge hinzufügen. Wir prüfen neue Einträge und schalten diese frei, bevor sie auf der Karte erscheinen.
+              Sie können weitere Einträge hinzufügen. Wir prüfen neue Einträge
+              und schalten diese frei, bevor sie auf der Karte erscheinen.
             </p>
             <Link
               to={'/add'}
-              className='ui primary fluid button'
-              style={{marginTop: '1rem'}}
+              className="ui primary fluid button"
+              style={{ marginTop: '1rem' }}
             >
               Eintrag erfassen
             </Link>
             <h2>Wie kann ich mich austragen lassen?</h2>
             <p>
-              Sie sind auf der Karte und wollen das garnicht? Schreiben sie einfach eine E-Mail an {' '}
+              Sie sind auf der Karte und wollen das garnicht? Schreiben sie
+              einfach eine E-Mail an{' '}
               <a
                 href={`mailto:${data.site.siteMetadata.email}`}
-                target='_blank'
-                rel='noopener noreferrer'
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 {data.site.siteMetadata.email}
               </a>
